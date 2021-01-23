6496f11a4d2bbb1fb38d7095cb3b7e686993f7d5
Pboerma <boerma.pieter@gmail.com>
2021-01-23 10:54:15 -0500

    re-pushed PR#7 without schema

---
+++ config/connection.js
@@ -0,0 +1,19 @@
+// Connecting to mysql
+// Dependencies
+const Sequelize = require('sequelize');
+
+// Creates mySQL connection for employeedb
+
+const sequelize = new Sequelize('employee', 'root', '', {
+  host: 'localhost',
+  port: 3306,
+  dialect: 'mysql',
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000,
+  },
+});
+
+// Exports the connection for other files to use
+module.exports = sequelize;
