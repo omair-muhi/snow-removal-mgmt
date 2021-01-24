0039dda856c024341a4a3749d2313cdaa6acba90
MT2314 <39105763+MT2314@users.noreply.github.com>
2021-01-24 18:31:22 -0500

    removed passport files

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ const Sequelize = require('sequelize');
 
 // Creates mySQL connection for employeedb
 
-const sequelize = new Sequelize('', 'root', '', {
+const sequelize = new Sequelize('snow_removal_db' 'root', 'mikeServer1', {
   host: 'localhost',
   port: 3306,
   dialect: 'mysql',
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,9 +1,6 @@
 // Requiring path to so we can use relative routes to our HTML files
 const path = require("path");
 
-// Requiring our custom middleware for checking if a user is logged in
-const isAuthenticated = require("../config/middleware/isAuthenticated");
-
 module.exports = function(app) {
   app.get("/", (req, res) => {
     // If the user already has an account send them to the members page
@@ -18,12 +15,12 @@ module.exports = function(app) {
     if (req.user) {
       res.redirect("/members");
     }
-    res.sendFile(path.join(__dirname, "../public/login.html"));
+    res.sendFile(path.join(__dirname, "../public/crew.html"));
   });
 
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
-  app.get("/members", isAuthenticated, (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/members.html"));
+  app.get("/manager", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/manager.html"));
   });
 };
