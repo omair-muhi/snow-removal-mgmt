c0c77181e23971fedc1d93f4db2ac88b76d2b2e5
Omair Muhi <omairmuhi@icloud.com>
2021-01-24 08:23:19 -0500

    Subject: Add CRUD code for Crew table Problem: NA Solution: NA Note: NA

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,53 +1,47 @@
-// Requiring our models and passport as we've configured it
-const db = require("../models");
-const passport = require("../config/passport");
+// Requiring our Todo model
+const db = require('../models');
 
-module.exports = function(app) {
-  // Using the passport.authenticate middleware with our local strategy.
-  // If the user has valid login credentials, send them to the members page.
-  // Otherwise the user will be sent an error
-  app.post("/api/login", passport.authenticate("local"), (req, res) => {
-    // Sending back a password, even a hashed password, isn't a good idea
-    res.json({
-      email: req.user.email,
-      id: req.user.id
+// Routes - Crews - code adapted from Week #14 Activity #12 Blog CRUD
+// =============================================================
+module.exports = (app) => {
+    // GET route for getting all of the crews
+    app.get('/api/crews/', (req, res) => {
+        db.Crew.findAll({}).then((dbPost) => res.json(dbPost));
     });
-  });
-
-  // Route for signing up a user. The user's password is automatically hashed and stored securely thanks to
-  // how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
-  // otherwise send back an error
-  app.post("/api/signup", (req, res) => {
-    db.User.create({
-      email: req.body.email,
-      password: req.body.password
-    })
-      .then(() => {
-        res.redirect(307, "/api/login");
-      })
-      .catch(err => {
-        res.status(401).json(err);
-      });
-  });
-
-  // Route for logging user out
-  app.get("/logout", (req, res) => {
-    req.logout();
-    res.redirect("/");
-  });
-
-  // Route for getting some data about our user to be used client side
-  app.get("/api/user_data", (req, res) => {
-    if (!req.user) {
-      // The user is not logged in, send back an empty object
-      res.json({});
-    } else {
-      // Otherwise send back the user's email and id
-      // Sending back a password, even a hashed password, isn't a good idea
-      res.json({
-        email: req.user.email,
-        id: req.user.id
-      });
-    }
-  });
+    // GET route for retrieving a single crew
+    app.get('/api/crews/:id', (req, res) => {
+        db.Crew.findOne({
+            where: {
+                id: req.params.id,
+            },
+        }).then((dbPost) => res.json(dbPost));
+    });
+    // POST route for creating a new crew
+    app.post('/api/crews', (req, res) => {
+        console.log(req.body);
+        db.Crew.create({
+            employee_id: req.body.employee_id,
+            job_id: req.body.job_id,
+        }).then((dbPost) => res.json(dbPost));
+    });
+    // DELETE route for deleting crews
+    app.delete('/api/crews/:id', (req, res) => {
+        db.Crew.destroy({
+            where: {
+                id: req.params.id,
+            },
+        }).then((dbPost) => res.json(dbPost));
+    });
+    // PUT route for updating crews
+    app.put('/api/crews', (req, res) => {
+        db.Crew.update(req.body, {
+            where: {
+                id: req.body.id,
+            },
+        }).then((dbPost) => res.json(dbPost));
+    });
 };
+// Routes - Employees
+// =============================================================
+// Routes - Jobs
+// =============================================================
