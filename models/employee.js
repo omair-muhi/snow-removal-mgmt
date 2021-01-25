ce812f059aba601b181929e25db16b1dc6e997aa
MT2314 <39105763+MT2314@users.noreply.github.com>
2021-01-25 14:34:13 -0500

    Model Update to include Admin

--- models/employee.js
+++ models/employee.js
@@ -1,34 +1,37 @@
 const Sequelize = require("sequelize");
 // Creating our Employee Model
-module.exports = function(sequelize, DataTypes) {
-    const Employee = sequelize.define("Employee", {
-      // Name cannot be null
-      id: {
-          type: Sequelize.INTEGER,
-          primaryKey: true,
-          autoIncrement: true
-      },
-      Name: {
-        type: Sequelize.STRING,
-        allowNull: false,
-        unique: false,
-        freezeTableName: true
-      },
-      
-      Title: {
-        type: Sequelize.STRING,
-        allowNull: false,
-        unique: false,
-        freezeTableName: true
-      },
-      Contact: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        unique: true,
-        freezeTableName: true
-      },
+module.exports = function (sequelize, DataTypes) {
+  const Employee = sequelize.define("Employee", {
+    // Name cannot be null
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    Name: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: false,
+      freezeTableName: true
+    },
 
-    });
-    return Employee;
-  };
-  
+    Title: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: false,
+      freezeTableName: true
+    },
+    Contact: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      unique: true,
+      freezeTableName: true
+    },
+    Admin: {
+      type: Sequelize.BOOLEAN,
+      allowNull: false,
+      freezeTableName: true
+    }
+  });
+  return Employee;
+};
