4e8849190b34f656b77050ef53418b22717a2feb
MT2314 <39105763+MT2314@users.noreply.github.com>
2021-01-25 15:09:28 -0500

    fixed migration +timestamp

--- migrations/20210125030458-employee.js
+++ migrations/20210125030458-employee.js
@@ -22,7 +22,7 @@ module.exports = {
       freezeTableName: true
     },
     Contact: {
-      type: Sequelize.INTEGER,
+      type: Sequelize.STRING,
       allowNull: false,
       unique: true,
       freezeTableName: true
