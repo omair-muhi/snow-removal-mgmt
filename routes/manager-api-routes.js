2dab2b9375e0415451fed62b8eb8de78f5328f4d
Omair Muhi <omairmuhi@icloud.com>
2021-01-30 01:08:07 -0500

    Subject: Set up front-end for manager overview Problem: NA Solution: NA Note: To allow creation of crews

--- routes/manager-api-routes.js
+++ routes/manager-api-routes.js
@@ -7,14 +7,17 @@ module.exports = (app) => {
     // route for HBS    
     app.get('/managerOverviewMain', (req, res) => {
         console.log("Hit /managerOverviewMain end-point!");
-        db.Employee.findAll({}).then((dbPost) => {
+        db.Employee.findAll({}).then((allEmployees) => {
             // console.log(dbPost[0].dataValues);
-            const hbsObject = {
-                employees: dbPost,
-            };
-            console.log(hbsObject.employees[0].dataValues);
-            app.engine('handlebars', exphbs({ defaultLayout: 'managerOverviewMain' }));
-            res.render('managerOverviewPartial', hbsObject);
+            // console.log(hbsObject.employees[0].dataValues);
+            db.Job.findAll({}).then((allJobs) => {
+                const hbsObject = {
+                    employees: allEmployees,
+                    jobs: allJobs
+                };
+                app.engine('handlebars', exphbs({ defaultLayout: 'managerOverviewMain' }));
+                res.render('managerOverviewPartial', hbsObject);
+            })
         });
     });
 };
